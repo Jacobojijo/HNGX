f0610728413c31b356e4371dbdfbd1bd35311d2a
Jacobojijo <jacobojijo2017@gmail.com>
2023-09-08 12:31:07 +0300

    small change

--- task1/endpoint.js
+++ task1/endpoint.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port_number = process.env.port_number || 3000;
+const port_number = process.env.port_number || 8000;
 
 app.listen(port_number);
 const days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -12,7 +12,7 @@ app.get('/api', (req, res) => {
     const slack_name = req.query.slack_name;
     const track = req.query.track;
     const current_day = days_of_week[array_of_day_week];
-    const utc_time = new Date().toISOString();
+    const utc_time = new Date().toUTCString();
     const github_file_url = "https://github.com/Jacobojijo/HNGX/blob/main/task1/endpoint.js";
     const github_repo_url = "https://github.com/Jacobojijo/HNGX";
     const status_code = 200;
